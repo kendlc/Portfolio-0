c016732cab9e6e3899d44b550d0055fecf87205d
kendlc <kennethcdlc@gmail.com>
2022-06-09 14:21:54 +1000

    added gradient background to navbar and media query

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,12 +1,13 @@
 import React from "react";
+import "./Navigation.css";
 
 const Navigation = () => {
 
     return (
         <div>
-            <nav className="navbar navbar-expand-lg mt-4 fixed-top container bgOnSmall"
-            style={{fontSize: '20px', fontWeight: 'bold',color: '#9244ED'}}>
-                <div className="container-fluid">
+            <nav className="navgtr navbar navbar-expand-lg fixed-top bgOnSmall"
+            style={{fontSize: '20px', fontWeight: 'bold', height: '100px'}}>
+                <div className="container container-fluid" style={{marginTop: '-25px'}}>
                     <a className="navbar-brand" href="/">
                         <img src="./images/icon.png" width={45} alt="ken"/>
                     </a>
@@ -15,36 +16,36 @@ const Navigation = () => {
                     </button>
 
 
-                    <div className="offcanvas offcanvas-end" tabIndex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel"
-                    style={{backgroundColor: 'rgba(255,255,255,0.8)'}}>
+                    <div className="offcanvas offcanvas-end navOffset" tabIndex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel"
+                    >
                     <div className="offcanvas-header">
                         
                         <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                     </div>
                     <div className="offcanvas-body">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0 justify-content-end flex-grow-1 pe-3">
-                        <li className="nav-item">
+                        <li className="nav-item ">
                             <a className="nav-link" aria-current="page" href="#about">
                                 <img src="./images/svg/person.svg" width={25} alt="About" className="mb-1 mx-1"/>
-                                <span className="mx-2">About</span>
+                                <span className="mx-2 text-light">About</span>
                                 </a>
                         </li>
                         <li className="nav-item">
                             <a className="nav-link" aria-current="page" href="#projects">
                                 <img src="./images/svg/file-earmark-code.svg" width={25} alt="About" className="mb-1 mx-1"/>
-                                <span className="mx-2">Projects</span>
+                                <span className="mx-2 text-light">Projects</span>
                             </a>
                         </li>
                         <li className="nav-item">
                             <a className="nav-link" aria-current="page" href="#contact">
                                 <img src="./images/svg/chat-left-dots.svg" width={25} alt="About" className="mb-1 mx-1"/>
-                                <span className="mx-2">Contact</span>
+                                <span className="mx-2 text-light">Contact</span>
                             </a>
                         </li>
                         <li className="nav-item">
                             <a className="nav-link" aria-current="page" href="./KenDelaCruz-Resume.pdf" download>
                                 <img src="./images/svg/file-earmark-pdf.svg" width={25} alt="About" className="mb-1 mx-1"/>
-                                <span className="mx-2">CV</span>
+                                <span className="mx-2 text-light">CV</span>
                             </a>
                         </li>
                     </ul>
