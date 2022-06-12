99ea5f2124b014ec070356adf54de4eb1dcee9a4
kendlc <kennethcdlc@gmail.com>
2022-06-12 18:25:57 +1000

    added filter project section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -12,20 +12,49 @@ const Projects = () => {
                 </div>
             </div>
 
-            {/* <div className="row d-flex d-flex justify-content-center">
-                <div className="col-sm-5">
+            <div className="row d-flex d-flex justify-content-center">
+                <div className="col-sm-6"
+                style={{zIndex: '-999'}}>
                     <img src="./images/projects/p0.png" alt="project 0"
                     className="img-fluid"
+                    style={{borderRadius: '5px', filter: "sepia(100%) hue-rotate(190deg) saturate(500%)", opacity: '0.7'}}
                     />
                     
                 </div>
-                <div className="col-sm-5">
-                <img src="./images/projects/p1.png" alt="project 0"
+                <div className="col-sm-4"
+                style={{color: '#47177F', textAlign: 'left'}}>
+                {/* <img src="./images/projects/p1.png" alt="project 0"
                     className="img-fluid"
                     style={{marginLeft: '-10rem'}}
-                    />
+                    /> */}
+                <h4>: 4</h4>
+                <a href="https://manga-shoppu.herokuapp.com/" 
+                className="mx-2 text-decoration-none text-reset" target="_blank" rel="noreferrer">
+                    <h2>Pokemon Quizzr</h2>
+                </a>
+                <div>
+                    <p
+                    style={{background: '#716FD8',padding: '1rem', borderRadius: '5px', width: '110%', marginLeft: '-4rem',color: 'white'}}>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe officiis quisquam, reiciendis quasi explicabo quam quos non odio laboriosam mollitia at quibusdam, deleniti ipsum? Quae rerum dolore assumenda nobis iusto?
+                    </p>
+                    <p>
+                        ReactJS | Firebase | PokemonAPI | Bootstrap | CSS 
+                    </p>
+                    <p>
+                        <a href="https://github.com/kendlc/Project1-manga-shoppu" 
+                        className="mx-2" target="_blank" rel="noreferrer">
+                            <img src="./images/svg/github.svg" 
+                            width={25} alt="Github"/>
+                        </a>
+                        <a href="https://manga-shoppu.herokuapp.com/" 
+                        className="mx-2" target="_blank" rel="noreferrer">
+                            <img src="./images/svg/demo.svg" 
+                            width={25}alt="Live Site"/>
+                        </a>
+                    </p>
                 </div>
-            </div> */}
+                </div>
+            </div>
 
             <div className="row d-flex h-50">
                 <div className="col-sm-6 d-flex justify-content-center justify-content-sm-end align-self-center">
