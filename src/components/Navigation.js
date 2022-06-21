ab72346b255ddab8f7259a2d4291eba2e9d3bf75
kendlc <kennethcdlc@gmail.com>
2022-06-21 12:03:49 +1000

    offcanvas toggle on links

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navigation.css";
 
 const Navigation = () => {
+    const [visib, setVisib] = useState('');
 
     return (
         <div>
@@ -11,12 +12,15 @@ const Navigation = () => {
                     <a className="navbar-brand" href="/">
                         <img src="./images/icon.png" width={45} alt="ken"/>
                     </a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">
+                    <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight"
+                    onClick={ () => setVisib('block')}
+                    >
                     <span className="navbar-toggler-icon"></span>
                     </button>
 
 
                     <div className="offcanvas offcanvas-end navOffset" tabIndex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel"
+                    style={{display: visib}}
                     >
                     <div className="offcanvas-header">
                         
@@ -24,19 +28,23 @@ const Navigation = () => {
                     </div>
                     <div className="offcanvas-body">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0 justify-content-end flex-grow-1 pe-3">
-                        <li className="nav-item">
-                            <a className="nav-link " aria-current="page" href="#about" >
-                                <img src="./images/svg/person.svg" width={25} alt="About" className="mb-1 mx-1"/>
-                                <span className="mx-2 text-light">About</span>
+                        <li className="nav-item"
+                        onClick={ () => setVisib('none')}
+                        >
+                            <a className="nav-link " aria-current="page" href="#about">
+                                    <img src="./images/svg/person.svg" width={25} alt="About" className="mb-1 mx-1"/>
+                                    <span className="mx-2 text-light">About</span>
                             </a>
                         </li>
-                        <li className="nav-item">
+                        <li className="nav-item"
+                        onClick={ () => setVisib('none')}>
                             <a className="nav-link" aria-current="page" href="#projects">
                                 <img src="./images/svg/file-earmark-code.svg" width={25} alt="Projects" className="mb-1 mx-1"/>
                                 <span className="mx-2 text-light">Projects</span>
                             </a>
                         </li>
-                        <li className="nav-item">
+                        <li className="nav-item"
+                        onClick={ () => setVisib('none')}>
                             <a className="nav-link" aria-current="page" href="#contact">
                                 <img src="./images/svg/chat-left-dots.svg" width={25} alt="Contacts" className="mb-1 mx-1"/>
                                 <span className="mx-2 text-light">Contact</span>
