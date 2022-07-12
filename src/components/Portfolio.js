c82c3145f6e05c0d5dd8f317b18ae01f47b8dbd5
kendlc <kennethcdlc@gmail.com>
2022-07-12 22:11:06 +1000

    delay loading on projects

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import About from "./About";
 import Contact from "./Contact";
 import Home from "./Home";
@@ -6,13 +6,22 @@ import Navigation from "./Navigation";
 import Projects from "./Projects";
 
 const Portfolio = () => {
+    const [delay, setDelay] = useState(false);
+
+    useEffect( () => {
+        setTimeout( () => {
+            setDelay(true);
+        },500)
+    },[]);
 
     return (
         <div>
             <Navigation />
             <Home />
             <About />
-            <Projects />
+            { delay &&
+                <Projects />
+            }
             <Contact />
         </div>
     );
