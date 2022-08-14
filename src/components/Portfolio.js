48b8b3c53f3afd04fdb02afe7a8b5d60ab2bc236
kendlc <kennethcdlc@gmail.com>
2022-08-14 14:01:54 +1000

    added loading screen

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -7,22 +7,44 @@ import Projects from "./Projects";
 
 const Portfolio = () => {
     const [delay, setDelay] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect( () => {
         setTimeout( () => {
             setDelay(true);
-        },500)
+        },100)
+        setTimeout( () => {
+            setIsLoading(false);
+        }, 2100)
     },[]);
 
     return (
         <div>
-            <Navigation />
-            <Home />
-            <About />
-            { delay &&
-                <Projects />
+            {!isLoading ? 
+                <div>
+                    <Navigation />
+                    <Home />
+                    <About />
+                    { delay &&
+                        <Projects />
+                    }
+                    <Contact />
+                </div>
+            : 
+                <div>
+                    <div className="loadingIcon"
+                    style={{
+                        marginTop: '35vh',
+                        marginBottom: '200px',
+                    }}>
+                        <img src="./images/icon.png" alt="circle" draggable="false"
+                        style={{
+                            zIndex: '1', 
+                            width: '150px',
+                            }} data-aos="zoom-in"/>
+                    </div>
+                </div>
             }
-            <Contact />
         </div>
     );
 }
