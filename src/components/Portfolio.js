c9da72592ecfd67ebfbe571e480b8440c998c613
kendlc <kennethcdlc@gmail.com>
2022-08-14 14:20:55 +1000

    background fade in

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -13,7 +13,7 @@ const Portfolio = () => {
     useEffect( () => {
         setTimeout( () => {
             setDelay(true);
-        },100)
+        },500)
     
         setTimeout( () => {
             setIsLoading(false);
