e14a15f732905a63eda189df5c48e4b3f66372ea
kendlc <kennethcdlc@gmail.com>
2024-04-05 21:39:14 +1100

    loadingbg image when on loading screen

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -6,49 +6,65 @@ import Navigation from "./Navigation";
 import Projects from "./Projects";
 
 const Portfolio = () => {
-    const [delay, setDelay] = useState(false);
-    const [isLoading, setIsLoading] = useState(true);
+  const [delay, setDelay] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
+  useEffect(() => {
+    setTimeout(() => {
+      setDelay(true);
+    }, 500);
+    const loadBgImage = async () => {
+      addImageProcess();
+    };
+    loadBgImage();
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1900);
+  }, []);
 
-    useEffect( () => {
-        setTimeout( () => {
-            setDelay(true);
-        },500)
-    
-        setTimeout( () => {
-            setIsLoading(false);
-        }, 1900)
-    },[]);
+  const addImageProcess = () => {
+    return new Promise((resolve, reject) => {
+      let img = new Image();
+      img.onload = () => resolve(img.height);
+      img.onerror = reject;
+      img.src = "./images/bg11.png";
+    });
+  };
 
-    return (
+  return (
+    <div>
+      {!isLoading ? (
         <div>
-            {!isLoading ? 
-                <div>
-                    <Navigation />
-                    <Home />
-                    <About />
-                    { delay &&
-                        <Projects />
-                    }
-                    <Contact />
-                </div>
-            : 
-                <div>
-                    <div className="loadingIcon"
-                    style={{
-                        marginTop: '35vh',
-                        marginBottom: '200px',
-                    }}>
-                        <img src="./images/icon.png" alt="circle" draggable="false"
-                        style={{
-                            zIndex: '1', 
-                            width: '150px',
-                            }} data-aos="zoom-in"/>
-                    </div>
-                </div>
-            }
+          <Navigation />
+          <Home />
+          <About />
+          {delay && <Projects />}
+          <Contact />
         </div>
-    );
-}
+      ) : (
+        <div>
+          <div
+            className="loadingIcon"
+            style={{
+              marginTop: "35vh",
+              marginBottom: "200px",
+            }}
+          >
+            <img
+              src="./images/icon.png"
+              alt="circle"
+              draggable="false"
+              style={{
+                zIndex: "1",
+                width: "150px",
+              }}
+              data-aos="zoom-in"
+            />
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default Portfolio;
